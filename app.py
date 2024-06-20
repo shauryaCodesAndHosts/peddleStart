3cc3fb5b8689ce26256a8800841ddc8bf747a436
Shaurya Yamdagni <122702840+shauryaCodesAndHosts@users.noreply.github.com>
2024-06-20 17:51:11 +0530

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ api.add_resource(TaskList, '/tasks')
 api.add_resource(TaskResource, '/tasks/<int:task_id>')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    # app.run(host='0.0.0.0', port='5000')
+    app.run(debug=true)
